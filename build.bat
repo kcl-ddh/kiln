f98a379a232523a956924b2a6cef6800e1d0b2fd
Jamie Norrish <jamie@artefact.org.nz>
2011-12-16 11:08:23 +1300

    Added Sesame to Kiln as another webapp.

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
+set ANT_OPTS='-XX:PermSize=64m -XX:MaxPermSize=128m -Xmx512m -Dinfo.aduna.platform.appdata.basedir=.\webapps\sesame\app_dir\'
+
 sw\ant\bin\ant.bat -f local.build.xml %1 %2 %3 %4 %5 %6 %7 %8 %9
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export ANT_OPTS='-XX:PermSize=64m -XX:MaxPermSize=128m -Xmx512m'
+export ANT_OPTS='-XX:PermSize=64m -XX:MaxPermSize=128m -Xmx512m -Dinfo.aduna.platform.appdata.basedir=./webapps/sesame/app_dir/'
 
 sw/ant/bin/ant -f local.build.xml $*
