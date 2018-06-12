9fe2130c549b6f2a965ee6ff6987b9c262d35387
Jamie Norrish <jamie@artefact.org.nz>
2018-06-13 09:46:51 +1200

    Upgraded Sesame to rdf4j version 2.3.2.

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-set ANT_OPTS=-XX:PermSize=64m -XX:MaxPermSize=128m -Xmx512m -Dinfo.aduna.platform.appdata.basedir=.\webapps\openrdf-sesame\app_dir\ -Dorg.eclipse.jetty.LEVEL=WARN
+set ANT_OPTS=-Xmx512m -Dorg.eclipse.rdf4j.appdata.basedir=.\webapps\rdf4j-server\app_dir\ -Dorg.eclipse.jetty.LEVEL=WARN
 
 sw\ant\bin\ant.bat -S -f local.build.xml %1 %2 %3 %4 %5 %6 %7 %8 %9
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export ANT_OPTS='-XX:PermSize=64m -XX:MaxPermSize=128m -Xmx512m -Dinfo.aduna.platform.appdata.basedir=./webapps/openrdf-sesame/app_dir/ -Dorg.eclipse.jetty.LEVEL=WARN'
+export ANT_OPTS='-Xmx512m -Dorg.eclipse.rdf4j.appdata.basedir=./webapps/rdf4j-server/app_dir/ -Dorg.eclipse.jetty.LEVEL=WARN'
 
 sw/ant/bin/ant -S -f local.build.xml $*
